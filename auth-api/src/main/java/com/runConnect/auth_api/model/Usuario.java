87083b9cce3a69ea49a4865d97dd2d3629eb7533
Felipe lima <140981807+FelipeLima2003@users.noreply.github.com>
2025-10-08 16:55:06 -0300

    Criação-Feature-Cadastro,atualização na model de usuário

--- auth-api/src/main/java/com/runConnect/auth_api/model/Usuario.java
+++ auth-api/src/main/java/com/runConnect/auth_api/model/Usuario.java
@@ -1,29 +1,41 @@
 package com.runConnect.auth_api.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.runConnect.auth_api.model.enums.Genero;
 
 import jakarta.persistence.*;
 import lombok.Data;
-
 @Data
 @Entity
 @Table(name = "USUARIO")
 public class Usuario {
+
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
- 
+    
     @Column(name = "ID")
     private Integer id;
 
     @Column(name = "NOME")
     private String nome;
 
+    @Column(name = "CPF")
+    private String cpf;
+
     @Column(name = "EMAIL")
     private String email;
+
+    @Column(name = "TELEFONE")
+    private String telefone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "GENERO")
+    private Genero genero;
 
     @JsonIgnore
     @Column(name = "SENHA")
     private String senha;
 
-    @Column(name = "CPF")
-    private String cpf;
+  
 }
