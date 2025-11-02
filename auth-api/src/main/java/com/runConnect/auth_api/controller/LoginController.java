8b61844d577ca0bf4626ce40a293ca696ca34e14
Felipe Lima <140981807+FelipeLima2003@users.noreply.github.com>
2025-11-01 21:22:13 -0300

    Refactor LoginController to use 'email' in login

--- auth-api/src/main/java/com/runConnect/auth_api/controller/LoginController.java
+++ auth-api/src/main/java/com/runConnect/auth_api/controller/LoginController.java
@@ -38,7 +38,7 @@ public class LoginController {
     // Login do usuario
     @GetMapping("/login")
     public ResponseEntity<List<Usuario>> login(
-     @RequestParam("usuario") String email,
+     @RequestParam("email") String email,
      @RequestParam("senha") String senha) 
      {
         Optional<Usuario> usuarioOptional = usuarioRepository.findByEmailAndSenha(email, senha);
@@ -134,5 +134,6 @@ public class LoginController {
                 .collect(Collectors.toList());
         return ResponseEntity.ok(seguindoDTO);
     }
+
 
 }
