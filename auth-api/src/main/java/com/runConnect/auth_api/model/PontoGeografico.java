b475ce1f6aa537c7803e9b56de32db011fd836ea
Felipe Lima <140981807+FelipeLima2003@users.noreply.github.com>
2025-11-11 20:59:21 -0300

    Add imports and update PontoGeografico class

--- auth-api/src/main/java/com/runConnect/auth_api/model/PontoGeografico.java
+++ auth-api/src/main/java/com/runConnect/auth_api/model/PontoGeografico.java
@@ -1,4 +1,16 @@
 package com.example.seuprojeto.model;
+import java.math.BigDecimal;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import java.sql.Timestamp;
 
 public class PontoGeografico {
 
