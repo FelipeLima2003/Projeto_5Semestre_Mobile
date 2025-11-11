136bf3f7f6319fa9bbddeb0751054ab845b3c6c0
Felipe Lima <140981807+FelipeLima2003@users.noreply.github.com>
2025-11-11 20:58:55 -0300

    Create PontoGeografico class with coordinates
    Added PontoGeografico class with properties and comments.

---
+++ auth-api/src/main/java/com/runConnect/auth_api/model/PontoGeografico.java
@@ -0,0 +1,12 @@
+package com.example.seuprojeto.model;
+
+public class PontoGeografico {
+
+    private int pontoId;
+    private String localNome;
+    private float coordX;
+    private float coordY;
+
+    // Construtores, Getters e Setters aqui
+    // Se estiver usando Lombok, use @Data e @AllArgsConstructor
+}
