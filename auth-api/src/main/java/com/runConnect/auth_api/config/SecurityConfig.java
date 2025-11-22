b6059da558ea978f6af29dac23e9103377d00c04
Felipe lima <140981807+FelipeLima2003@users.noreply.github.com>
2025-11-22 18:09:33 -0300

    Fix: Bug:filtro de endpoints e id de seguidores

--- auth-api/src/main/java/com/runConnect/auth_api/config/SecurityConfig.java
+++ auth-api/src/main/java/com/runConnect/auth_api/config/SecurityConfig.java
@@ -34,7 +34,6 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/usuario/{id}").permitAll()
                         .requestMatchers(HttpMethod.POST, "/usuario/{idSeguido}/seguir").permitAll()
                         .requestMatchers(HttpMethod.POST, "/usuario/{idSeguido}/deixar-de-seguir").permitAll()
-                        .anyRequest().authenticated()
                         .requestMatchers(HttpMethod.GET, "/usuario/{idSeguido}/seguidores").permitAll()
                         .requestMatchers(HttpMethod.GET, "/usuario/{idSeguido}/seguindo").permitAll()
                         .requestMatchers(HttpMethod.PUT, "/usuario/{idSeguido}/descricao").permitAll()
@@ -44,7 +43,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/corridas/{id}").permitAll()
                         .requestMatchers(HttpMethod.GET, "/corridas/usuario/{usuarioId}").permitAll()
                         .requestMatchers(HttpMethod.POST, "/uploads/imagem").permitAll()
-
+                        .anyRequest().authenticated() 
+                        // Qualquer outra requisição precisa estar autenticada
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class) // Adiciona nosso filtro antes do padrão
                 .build();
