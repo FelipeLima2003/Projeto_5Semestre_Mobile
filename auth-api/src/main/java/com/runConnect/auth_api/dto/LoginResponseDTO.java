7c1c46f88f6b9fab50ce3a7b89ff267a26524807
Gustavo Ramos Santos <114708113+lRAMOSl@users.noreply.github.com>
2025-11-22 23:14:01 -0300

    Adicionado os campos do usuário na DTO

--- auth-api/src/main/java/com/runConnect/auth_api/dto/LoginResponseDTO.java
+++ auth-api/src/main/java/com/runConnect/auth_api/dto/LoginResponseDTO.java
@@ -1,3 +1,10 @@
 package com.runConnect.auth_api.dto;
 
-public record LoginResponseDTO(String token) {}
+public record LoginResponseDTO(
+    String token,
+    Long id,       
+    String nome,
+    String email,
+    String cpf
+) {
+}
