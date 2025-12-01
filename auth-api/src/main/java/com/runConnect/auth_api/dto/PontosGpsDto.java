8791cb09b4163ca2863bb5750a3747425f71b055
Felipe Lima <140981807+FelipeLima2003@users.noreply.github.com>
2025-11-30 22:23:15 -0300

    Delete auth-api directory

--- auth-api/src/main/java/com/runConnect/auth_api/dto/PontosGpsDto.java
+++
@@ -1,9 +0,0 @@
-package com.runConnect.auth_api.dto;
-
-import java.math.BigDecimal;
-
-public record PontosGpsDto(
-    BigDecimal latitude,
-    BigDecimal longitude,
-    Integer ordem
-) {}
--- auth-api/src/main/java/com/runConnect/auth_api/model/enums/Genero.java
+++
@@ -1,7 +0,0 @@
-package com.runConnect.auth_api.model.enums;
-
-public enum Genero {
-    MASCULINO,
-    FEMININO,
-    PREFIRO_NAO_INFORMAR
-}
