13727bdba8f6754d8a1c20305c1a918f7cc07dbe
Felipe Lima <140981807+FelipeLima2003@users.noreply.github.com>
2025-12-02 20:34:09 -0300

    Restrict access to upload endpoints
    Removed unrestricted access to all upload endpoints.

--- auth-api/src/main/java/com/runConnect/auth_api/config/SecurityConfig.java
+++ auth-api/src/main/java/com/runConnect/auth_api/config/SecurityConfig.java
@@ -43,7 +43,6 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/corridas/{id}").permitAll()
                         .requestMatchers(HttpMethod.GET, "/corridas/usuario/{usuarioId}").permitAll()
                         .requestMatchers(HttpMethod.POST, "/uploads/imagem").permitAll()
-                        .requestMatchers("/uploads/**").permitAll()
                         // Adicionar demais endpoints para liberar as requisições.
                         .anyRequest().authenticated() 
                          
