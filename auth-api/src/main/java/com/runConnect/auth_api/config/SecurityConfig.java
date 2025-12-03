ae7fc8acf69345748188c935a7d70cbe253a8705
Gustavo Ramos Santos <114708113+lRAMOSl@users.noreply.github.com>
2025-12-02 21:20:53 -0300

    Comment out SecurityConfig class and methods

--- auth-api/src/main/java/com/runConnect/auth_api/config/SecurityConfig.java
+++ auth-api/src/main/java/com/runConnect/auth_api/config/SecurityConfig.java
@@ -1,50 +1,50 @@
-package com.runConnect.auth_api.config;
+// package com.runConnect.auth_api.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.http.HttpMethod;
+// import org.springframework.security.authentication.AuthenticationManager;
+// import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.config.http.SessionCreationPolicy;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
+// @Configuration
+// @EnableWebSecurity
+// public class SecurityConfig {
     
-    @Autowired
-    private SecurityFilter securityFilter; 
+//     @Autowired
+//     private SecurityFilter securityFilter; 
 
-   @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(csrf -> csrf.disable()) 
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorize -> authorize
+//    @Bean
+//     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//         return http
+//                 .csrf(csrf -> csrf.disable()) 
+//                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                 .authorizeHttpRequests(authorize -> authorize
                         
-                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/cadastrar").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/usuario/cadastrar").permitAll() 
-                        .requestMatchers(HttpMethod.GET, "/usuario/login").permitAll() 
-                        .anyRequest().authenticated()
-                )
-                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
-    }
+//                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+//                         .requestMatchers(HttpMethod.POST, "/auth/cadastrar").permitAll()
+//                         .requestMatchers(HttpMethod.POST, "/usuario/cadastrar").permitAll() 
+//                         .requestMatchers(HttpMethod.GET, "/usuario/login").permitAll() 
+//                         .anyRequest().authenticated()
+//                 )
+//                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+//                 .build();
+//     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+//     @Bean
+//     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+//         return authenticationConfiguration.getAuthenticationManager();
+//     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-}
+//     @Bean
+//     public PasswordEncoder passwordEncoder() {
+//         return new BCryptPasswordEncoder();
+//     }
+// }
