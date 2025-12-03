40b85996df581b85902b0c4d7dd81195160ffaf4
Gustavo Ramos Santos <114708113+lRAMOSl@users.noreply.github.com>
2025-12-02 21:20:20 -0300

    Remove commented-out code in TokenService

--- auth-api/src/main/java/com/runConnect/auth_api/services/TokenService.java
+++ auth-api/src/main/java/com/runConnect/auth_api/services/TokenService.java
@@ -1,50 +1,50 @@
-package com.runConnect.auth_api.services;
+// package com.runConnect.auth_api.services;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
+// import java.time.Instant;
+// import java.time.LocalDateTime;
+// import java.time.ZoneOffset;
 
-import org.springframework.stereotype.Service;
+// import org.springframework.stereotype.Service;
 
-import com.runConnect.auth_api.model.Usuario;
+// import com.runConnect.auth_api.model.Usuario;
 
-import lombok.Value;
+// import lombok.Value;
 
-@Service
-public class TokenService {
+// @Service
+// public class TokenService {
     
-    @Value("$api.security.token.secret}")
-    private String secret;
+//     @Value("$api.security.token.secret}")
+//     private String secret;
 
-    public String gerarToken(Usuario usuario) {
+//     public String gerarToken(Usuario usuario) {
        
-        try {
-            Algorithm algoritmo = Algorithm.HMAC256(secret);
-            return JWT.create()
-                .withIssuer("runconnect-api")
-                .withSubject(usuario.getEmail())
-                .withExpiresAt(getExpirationDate())
-                .sign(algoritmo);
-        } catch (Exception e) {
+//         try {
+//             Algorithm algoritmo = Algorithm.HMAC256(secret);
+//             return JWT.create()
+//                 .withIssuer("runconnect-api")
+//                 .withSubject(usuario.getEmail())
+//                 .withExpiresAt(getExpirationDate())
+//                 .sign(algoritmo);
+//         } catch (Exception e) {
            
-        }
-    }
+//         }
+//     }
 
-    public String validarToken(String token){
-        try {
-            Algorithm algoritmo = Algorithm.HMAC256(secret);
-            return JWT.require(algoritmo)
-                .withIssuer("runconnect-api")
-                .build()
-                .verify(token)
-                .getSubject();
-        }catch (JWTVerificationException  exception) {
-            return "";
-        }
-    }
+//     public String validarToken(String token){
+//         try {
+//             Algorithm algoritmo = Algorithm.HMAC256(secret);
+//             return JWT.require(algoritmo)
+//                 .withIssuer("runconnect-api")
+//                 .build()
+//                 .verify(token)
+//                 .getSubject();
+//         }catch (JWTVerificationException  exception) {
+//             return "";
+//         }
+//     }
 
-    private Instant getExpirationDate() {
-        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
-    }
+//     private Instant getExpirationDate() {
+//         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+//     }
 
-}
+// }
